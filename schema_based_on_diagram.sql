4f8fc3fbdb778da7a6d4c68139b78a5fdc895ff5
yusufyusie <yesuf023@gmail.com>
2023-10-12 17:37:25 +0300

    Update database tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE DATABASE clinic;
--- Create medical_histories table
+-- Create a table named medical_histories with the following columns
 CREATE TABLE medical_histories (
     id INT PRIMARY KEY,
     admitted_at TIMESTAMP,
@@ -11,19 +11,19 @@ CREATE TABLE medical_histories (
 -- Add autoincremented PRIMARY KEY of id
 ALTER TABLE medical_histories
 ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
--- Create patients table
+-- Create a table named patients with the following columns
 CREATE TABLE patients (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     date_of_birth DATE
 );
--- Create treatments table
+-- Create a table named treatments with the following columns
 CREATE TABLE treatments (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     type varchar(255),
     name varchar(255),
 );
--- Create invoices table
+-- Create a table named invoices with the following columns
 CREATE TABLE invoices (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
      total_amount FLOAT,
@@ -32,7 +32,7 @@ CREATE TABLE invoices (
     medical_history_id INT,
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
 );
--- Create invoice_items table
+-- Create a table named invoice_items with the following columns
 CREATE TABLE invoice_items (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     unit_price DECIMAL,
