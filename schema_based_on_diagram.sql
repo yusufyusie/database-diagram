a21198d676b47c8672c65caa9ea05b55fa9fe449
yusufyusie <yesuf023@gmail.com>
2023-10-12 17:06:12 +0300

    Add  medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,6 +2,15 @@
 
 CREATE DATABASE clinic;
 -- Create medical_histories table
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status varchar(255)
+);
+-- Add autoincremented PRIMARY KEY of id
+ALTER TABLE medical_histories
+ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 -- Create patients table
 -- Create treatments table
 -- Create invoices table
