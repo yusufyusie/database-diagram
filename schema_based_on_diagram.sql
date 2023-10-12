28601e98c2eba98415082e0ff834a86326587fc7
yusufyusie <yesuf023@gmail.com>
2023-10-12 16:41:27 +0300

    Created clinic database

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,8 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE DATABASE clinic;
+-- Create medical_histories table
+-- Create patients table
+-- Create treatments table
+-- Create invoices table
+-- Create invoice_items table
